1c087e6d5d80dcdc392cce5710068ccec681dacc
ElColeoptero <samuelsaintomer62@gmail.com>
2021-06-18 09:07:56 +0200

    change makefile

---
+++ TP/src/proto.c
@@ -0,0 +1,6 @@
+#include "data.h" // On suit l'ordre des couches de manière croissante
+#include "proto.h"
+
+// Correspond à la couche 7 du modèle OSI (couche Application)
+
+// Traitement en fonction du type de requête reçue + fonctions venant de StreamCltSrv
---
+++ TP/src/proto.h
@@ -0,0 +1,10 @@
+#ifndef PROTO_H
+#define PROTO_H
+
+#include "reqRep.h"
+#include "data.h" // Potentiellement
+
+// Correspond à la couche 7 du modèle OSI (couche Application)
+
+
+#endif /* PROTO_H */
