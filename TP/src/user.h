3a758cc87c161a138a3692124bc326650bbafac6
Lafie-rage <corentin.destrez62@gmail.com>
2021-06-20 14:13:59 +0200

    Modifying user structure

--- TP/src/user.h
+++ TP/src/user.h
@@ -4,16 +4,18 @@
 #define MAX_SIZE_NAME 100
 #define MAX_USER 10
 
+#define EVERYONE_DESTIONATION_SOCKET 0
+
 typedef struct User {
-   char  name[MAX_SIZE_NAME];
-   int   socket;
-   char  talkTo[MAX_SIZE_NAME];
-} user_t; 
+   char name[MAX_SIZE_NAME];
+   int  socket;
+   int  destiantionSocket;
+} user_t;
 
 typedef struct Users
 {
 	user_t userList[MAX_CLIENT];
-	int nbUser;
+	int size;
 }users_t;
 
 #endif /* USER_H */
